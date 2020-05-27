139daa0cd3327f2b038af5c88206efb334f399cb
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-05-27 18:19:23 +0100

    Almost finished rarefaction script.

--- Code/RarefactionAndSubsampling.R
+++ Code/RarefactionAndSubsampling.R
@@ -279,117 +279,117 @@ utils::browseURL("https://paleobiodb.org/classic/displayCollResults?a=basicColle
 # We can see just how many collections are in our data set with:
 unique(RawData[, "collection_no"])
 
+# And format them into a list, recording both the stage they are assigned
+# to and the unique genera present:
+CollectionsList <- lapply(as.list(unique(RawData[, "collection_no"])),
+  function(x) {CollectionRows <- which(RawData[, "collection_no"] == x);
+  list(CollectionStage = RawData[CollectionRows[1], "early_interval"],
+  CollectionGenera = unique(unlist(lapply(strsplit(RawData[CollectionRows,
+  "genus"], split = " "), function(y) y[1]))))})
+
+# Each value in the list thus looks something like this:
+CollectionsList[[1]]
+
+# So what we want to do now is (for each geologic stage) randomly sample
+# from the available collections of that age and record the number of
+# unique genera we find.
+#
+# We can find all the Induan collections with...:
+which(unlist(lapply(CollectionsList, function(x)
+  x$CollectionStage == "Induan")))
+
+# ...and hence we can bootstrap this list with...:
+base::sample(which(unlist(lapply(CollectionsList, function(x)
+  x$CollectionStage == "Induan"))), replace = TRUE)
+
+# ..and grab just the genus names with...:
+unlist(lapply(CollectionsList[base::sample(which(unlist(lapply(CollectionsList,
+  function(x) x$CollectionStage == "Induan"))), replace = TRUE)], function(y)
+  y$CollectionGenera))
+
+# ..and finally the unique names with:
+unique(unlist(lapply(CollectionsList[base::sample(which(unlist(lapply(
+  CollectionsList, function(x) x$CollectionStage == "Induan"))),
+  replace = TRUE)], function(y) y$CollectionGenera)))
+
+# However, if we are going to repat this for all stages we also need to set a
+# sampling level (number of collections). One of the useful things about
+# bootstrapping is that we are not constrained by the size of the sample, if
+# we are happy to sample collections multiple times then we can sample more
+# than the total size of the sample. E.g., with our simple numerical example
+# we can sample the numbers 1 to 10 20 times with the size= option:
+base::sample(1:10, replace = TRUE, size = 20)
+
+# However, we don't want to oversample as this can create bias in our results.
+# As our Induan stage is the least well sampled we can use the number of
+# collections here as a guide to what is appropriate:
+length(which(unlist(lapply(CollectionsList, function(x)
+  x$CollectionStage == "Induan"))))
+
+# Fifty thus seems reasonable:
+SamplingLevel <- 50
+
+# To set this for our Induan stage we can add the size= option with:
+unique(unlist(lapply(CollectionsList[base::sample(which(unlist(lapply(
+  CollectionsList, function(x) x$CollectionStage == "Induan"))),
+  replace = TRUE, size = SamplingLevel)], function(y) y$CollectionGenera)))
+
+# Now we just need some code to repeat this for each stage...:
+lapply(as.list(StageNames), function(z) unique(unlist(lapply(CollectionsList[
+  base::sample(which(unlist(lapply(CollectionsList, function(x)
+  x$CollectionStage == z))), replace = TRUE, size = SamplingLevel)],
+  function(y) y$CollectionGenera))))
+
+# ...and again for multiple replicates to get a mean:
+NReplicates <- 100
+BootstrappedCollections <- unlist(lapply(as.list(StageNames), function(z)
+  mean(unlist(lapply(as.list(1:NReplicates), function(q)
+  length(unique(unlist(lapply(CollectionsList[base::sample(
+  which(unlist(lapply(CollectionsList, function(x) x$CollectionStage == z))),
+  replace = TRUE, size = SamplingLevel)], function(y)
+  y$CollectionGenera)))))))))
+
+# We can plot this as a time series (as we have before) with:
+plot(x = StageMidpoints, y = BootstrappedCollections, xlab = "Time (Ma)",
+  ylab = "Genus richness", type = "l", xlim = c(max(StageMidpoints),
+  min(StageMidpoints)), ylim = c(1, max(unlist(MultiReSamples))), log = "y")
+points(x = StageMidpoints, y = BootstrappedCollections, pch = 20, cex = 2,
+  col = viridis::viridis(length(StageNames)))
+
+# To finish up we can compare some of these results side-by-side:
+par(mfrow = c(3, 1))
+plot(x = StageMidpoints, y = unlist(lapply(CleanData, function(x)
+  length(unique(x)))), xlab = "Time (Ma)", ylab = "Richness (N genera)",
+  type = "l", xlim = c(max(StageMidpoints), min(StageMidpoints)),
+  ylim = c(1, max(unlist(lapply(CleanData,
+  function(x) length(unique(x)))))), log = "y", main = "Raw sampled-in-bin")
+points(x = StageMidpoints, y = unlist(lapply(CleanData, function(x)
+  length(unique(x)))), pch = 20, cex = 2,
+  col = viridis::viridis(length(StageNames)))
+plot(x = StageMidpoints, y = unlist(lapply(MultiReSamples,
+  function(x) x[[SamplingLevel]])), xlab = "Time (Ma)",
+  ylab = "Genus richness", type = "l", xlim = c(max(StageMidpoints),
+  min(StageMidpoints)), ylim = c(1, max(unlist(MultiReSamples))), log = "y",
+  main = "Rarefied sampled-in-bin")
+points(x = StageMidpoints, y = unlist(lapply(MultiReSamples,
+  function(x) x[[SamplingLevel]])), pch = 20, cex = 2,
+  col = viridis::viridis(length(StageNames)))
+plot(x = StageMidpoints, y = BootstrappedCollections, xlab = "Time (Ma)",
+  ylab = "Genus richness", type = "l", xlim = c(max(StageMidpoints),
+  min(StageMidpoints)), ylim = c(1, max(unlist(MultiReSamples))), log = "y",
+  main = "Collection bootstrapped sampled-in-bin")
+points(x = StageMidpoints, y = BootstrappedCollections, pch = 20, cex = 2,
+  col = viridis::viridis(length(StageNames)))
 
 
-lapply(as.list(unique(RawData[, "collection_no"])), function(x) {CollectionRows <- which(RawData[, "collection_no"] == x)})
 
 
 
-
+# ADD BELOW TO THE SETUP SCRIPT:
 
 "https://paleobiodb.org/data1.2/"
 
 
 
 
-
-
-
-
-
-collections <- unique(tetrapods_trim$collection_no)
-
-#Create frames to store sampled collection numbers and the matching curve for each iteration
-sample_list <- data.frame()
-gradients <- data.frame()
-
-#Indicate number of bootstraps and reps required -> 250 for terrestrial, 30 for marine, 100 reps of each
-bootstraps <- 30
-reps <- 100
-
-for (x in 1:reps){
-  
-  print(x)
-  
-  #Create subsampled bootstrap of 100 occurrences (with replacement)
-  colls_to_sample <- base::sample(collections, bootstraps, replace = T)
-  subset <- tetrapods_trim[FALSE,]
-  for (k in 1:bootstraps){
-    one_coll <- filter(tetrapods_trim, collection_no == colls_to_sample[k])
-    subset <- rbind(subset, one_coll)
-  }
-  
-  #Filter unique taxa
-  #Create an empty dataset with PBDB column names to collect unique occurrences in
-  unique_by_bin <- tetrapods_trim[FALSE,]
-  
-  #Loop through each collection
-  #For that collection, retain species occurrences, then retain unique taxa at gradually
-  #   higher taxonomic levels which are not already represented in that collection
-  #   i.e. if there is an indeterminate dicynodont but no occurrences in that collection which
-  #   are dicynodonts but more specifically identified, the occurrence is retained
-  for (i in 1:(length(labels))) {
-    one_bin <- subset %>% filter(paleolat_code == labels[i])
-    for (j in 1:(nrow(one_bin))) {
-      if (nrow(one_bin) != 0)
-        if (one_bin$accepted_rank[j] == "species") unique_by_bin <- rbind(unique_by_bin, one_bin[j,]) else
-          if (!is.na(one_bin$genus[j]))
-            (if (one_bin$genus[j] %in% one_bin$genus[-j] == F) unique_by_bin <- rbind(unique_by_bin, one_bin[j,])) else
-              if (!is.na(one_bin$family[j]))
-                (if (one_bin$family[j] %in% one_bin$family[-j] == F) unique_by_bin <- rbind(unique_by_bin, one_bin[j,])) else
-                  if (!is.na(one_bin$order[j]))
-                    (if (one_bin$order[j] %in% one_bin$order[-j] == F) unique_by_bin <- rbind(unique_by_bin, one_bin[j,]))
-    }
-  }
-  
-  #Remove repeats of species names, to get one occurrence per unique species per substage
-  unique_by_bin <- distinct(unique_by_bin, accepted_name, paleolat_code, .keep_all = T)
-  
-  #Generate counts
-  raw_curve <- count(unique_by_bin, paleolat_code)
-  
-  #Fill in gaps
-  for(m in 1:length(labels)) {
-    if((labels[m] %in% raw_curve$paleolat_code) == FALSE) raw_curve <- rbind(raw_curve, c(labels[m], 0))
-  }
-  
-  #Reorder for ease of checking
-  raw_curve <- arrange(raw_curve, desc(paleolat_code))
-  raw_curve$paleolat_code <- as.numeric(as.character(raw_curve$paleolat_code))
-  
-  #Add collection sample and curve for each iteration to a list
-  sample_list <- rbind(sample_list, colls_to_sample)
-  gradients <- rbind(gradients, raw_curve$n)
-}
-
-names(sample_list) <- 1:bootstraps
-names(gradients) <- sort(labels, decreasing = T) #reorder labels to match order in output
-
-write.csv(sample_list, file = "data/Bootstrap sample list.csv") #write csv of sampled collections
-write.csv(gradients, file = "data/Bootstrap gradients.csv") #write csv of gradients produced for each bootstrap
-
-sample_list <- read_csv("data/Bootstrap samples LP terr.csv")
-gradients <- read_csv("data/Bootstrap gradients LP terr.csv")
-gradients <- gradients[,-1]
-
-#Generate summary statistics
-means <- colMeans(gradients)
-stand_devs <- apply(gradients, 2, sd)
-error <- qnorm(0.95)*(stand_devs/sqrt(reps))
-lower <- means - error
-upper <- means + error
-#Create data frame containing summary statistics ready for plotting
-to_plot <- cbind(labels = sort(labels, decreasing = T), means, lower, upper)
-to_plot <- data.frame(to_plot)
-
-#Designate colour, limegreen or blue
-colour <- "limegreen"
-
-#Plot mean raw richness across bootstraps, with 95% confidence intervals
-ggplot(to_plot, aes(x = labels, y = means, ymin = lower, ymax = upper)) +
-  geom_line(size = 1, col = colour) + geom_point(size = 2, col = colour) + geom_linerange(col = colour) +
-  labs(x = "Palaeolatitude", y = "Raw species richness count") + coord_flip() +
-  scale_x_continuous(limits = c(-90, 90)) + geom_vline(aes(xintercept = 0), colour = "black") +
-  scale_colour_manual(values = "green") +
-  theme_classic()
 
